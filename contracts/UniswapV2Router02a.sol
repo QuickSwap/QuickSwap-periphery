8d0363c1ba7f054a52e5dfd45886ebaacde6985b
Paul Barclay <paulbarclay@users.noreply.github.com>
2021-06-17 08:17:21 -0700

    Enable conversion on add/remove liquidity Enable QuickConverter to receive requests from approved contracts (not just tx.origin) Add converter.convert() to the end of addLiquidity, addLiquidityETH, and removeLiquidity

---
+++ contracts/UniswapV2Router02a.sol
@@ -0,0 +1,83 @@
+pragma solidity =0.6.12;
+
+import './UniswapV2Router02.sol';
+import './interfaces/IQuickConverter';
+
+contract UniswapV2Router02a is UniswapV2Router02 {
+
+    IQuickConverter public immutable converter;
+
+    constructor(address _factory, address _WETH, address _converter) public UniswapV2Router02(_factory, _WETH) {
+        converter = IQuickConverter(_converter);
+    }
+
+    // Identical to the regular addLiquidity, plus the last line of converter.convert
+    // We cannot override _addLiquidity instead because it doesn't call mint(), which must be called before convert()
+    function addLiquidity(
+        address tokenA,
+        address tokenB,
+        uint amountADesired,
+        uint amountBDesired,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline
+    ) external virtual override ensure(deadline) returns (uint amountA, uint amountB, uint liquidity) {
+        (amountA, amountB) = _addLiquidity(tokenA, tokenB, amountADesired, amountBDesired, amountAMin, amountBMin);
+        address pair = UniswapV2Library.pairFor(factory, tokenA, tokenB);
+        TransferHelper.safeTransferFrom(tokenA, msg.sender, pair, amountA);
+        TransferHelper.safeTransferFrom(tokenB, msg.sender, pair, amountB);
+        liquidity = IUniswapV2Pair(pair).mint(to);
+        converter.convert(tokenA, tokenB);
+    }
+
+    // Identical to the regular addLiquidityETH, plus the last line of converter.convert
+    // We cannot override _addLiquidity instead because it doesn't call mint(), which must be called before convert()
+    function addLiquidityETH(
+        address token,
+        uint amountTokenDesired,
+        uint amountTokenMin,
+        uint amountETHMin,
+        address to,
+        uint deadline
+    ) external virtual override payable ensure(deadline) returns (uint amountToken, uint amountETH, uint liquidity) {
+        (amountToken, amountETH) = _addLiquidity(
+            token,
+            WETH,
+            amountTokenDesired,
+            msg.value,
+            amountTokenMin,
+            amountETHMin
+        );
+        address pair = UniswapV2Library.pairFor(factory, token, WETH);
+        TransferHelper.safeTransferFrom(token, msg.sender, pair, amountToken);
+        IWETH(WETH).deposit{value: amountETH}();
+        assert(IWETH(WETH).transfer(pair, amountETH));
+        liquidity = IUniswapV2Pair(pair).mint(to);
+        // refund dust eth, if any
+        if (msg.value > amountETH) TransferHelper.safeTransferETH(msg.sender, msg.value - amountETH);
+        converter.convert(token, WETH);
+    }
+
+    // Identical to the regular removeLiquidity, plus the last line of converter.convert
+    // This calls burn(), so it's safe to just override this one.
+    // Any calls after this are just returning funds to the user, so don't affect the conversion
+    function removeLiquidity(
+        address tokenA,
+        address tokenB,
+        uint liquidity,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline
+    ) public virtual override ensure(deadline) returns (uint amountA, uint amountB) {
+        address pair = UniswapV2Library.pairFor(factory, tokenA, tokenB);
+        IUniswapV2Pair(pair).transferFrom(msg.sender, pair, liquidity); // send liquidity to pair
+        (uint amount0, uint amount1) = IUniswapV2Pair(pair).burn(to);
+        (address token0,) = UniswapV2Library.sortTokens(tokenA, tokenB);
+        (amountA, amountB) = tokenA == token0 ? (amount0, amount1) : (amount1, amount0);
+        require(amountA >= amountAMin, 'UniswapV2Router: INSUFFICIENT_A_AMOUNT');
+        require(amountB >= amountBMin, 'UniswapV2Router: INSUFFICIENT_B_AMOUNT');
+        converter.convert(tokenA, tokenB);
+    }
+}
